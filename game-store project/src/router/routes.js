1ba45cbee7fe11ac9c7aaa430103a119849a3f59
Joshua Yves Edellon <joshuadellon.jye@gmail.com>
2023-07-11 15:34:07 +0800

    added browse page

--- game-store project/src/router/routes.js
+++ game-store project/src/router/routes.js
@@ -5,6 +5,12 @@ const routes = [
     component: () => import('pages/game-store/HomePage.vue'),
    
   },
+  {
+    path: '/browse',
+    name: 'browse',
+    component: () => import('pages/game-store/BrowsePage.vue'),
+   
+  },
 
   // Always leave this as last one,
   // but you can also remove it
