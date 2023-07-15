357ca07c9efb2583683d78f918eadb63750c02ab
Joshua Yves Edellon <joshuadellon.jye@gmail.com>
2023-07-16 05:40:26 +0800

    Changed the color palette and added responsiveness

--- game-store project/src/pages/game-store/HomePage.vue
+++ game-store project/src/pages/game-store/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <section>
-    <div class="navbar row justify-between q-mx-lg q-pa-xs">
-      <div class="flex flex-center">
+    <div class="navbar row justify-between q-mx-md q-pa-xs">
+      <div class="q-ml-md">
         <a class="logo-title text-bold" href="/">bot</a>
       </div>
-      <div class="flex flex-center">
+      <div class="flex flex-center q-mr-md">
         <div v-for="(navbtns, index) in navbtns" :key="index" class="q-mx-sm">
           <q-btn
             dense
@@ -18,9 +18,9 @@
         </div>
       </div>
     </div>
-    <div class="sec-1 q-pa-md">
-      <div class="featured-container q-mx-md q-pa-md">
-        <div class="featured-carousel row justify-between">
+    <div class="sec-1 q-mt-xl">
+      <div class="featured-container q-ma-lg">
+        <div class="featured-carousel row justify-center">
           <div class="q-pa-md col-12 col-md-7">
             <q-carousel
               class="game-slides"
@@ -48,6 +48,11 @@
                 class="game-image"
                 img-src="https://media.vandal.net/i/1200x630/2-2023/202321112364948_1.jpg"
               />
+              <q-carousel-slide
+                :name="4"
+                class="game-image"
+                img-src="https://cdn.akamai.steamstatic.com/steam/apps/814380/capsule_616x353.jpg?t=1678991267"
+              />
             </q-carousel>
           </div>
 
@@ -68,14 +73,14 @@
             <div class="featured-btn-container text-center">
               <q-btn
                 no-caps
+                flat
                 v-for="button in buttons"
                 :key="button.label"
                 :icon="button.icon"
                 :label="button.label"
-                flat
                 rounded
                 size="20px"
-                class="q-pa-md"
+                class="featured-btn q-pa-md q-mx-md"
               />
             </div>
           </div>
@@ -84,13 +89,13 @@
     </div>
   </section>
   <section>
-    <div class="sec-2">
+    <div class="sec-2 q-ma-md">
       <div class="q-pa-sm q-mx-md">
-        <h4 class="q-mb-none q-mt-sm">Games on Sale</h4>
+        <h4 class="q-mx-md text-white">Games on Sale</h4>
       </div>
       <div class="card-container row justify-evenly">
         <q-card
-          class="my-card q-pa-md column justify-evenly q-mb-md"
+          class="my-card q-pa-md q-mb-md"
           v-for="card in cards"
           :key="card.id"
           dense
@@ -111,16 +116,15 @@
           </q-card-section>
         </q-card>
       </div>
-      <div class="free-container q-mx-xl q-pa-sm q-ma-xl">
-        <h5 class="q-mx-md q-my-sm">
-          <q-icon name="redeem" size="2rem" class="q-px-md q-mb-none" />Free
-          games on
+      <div class="free-container q-pa-md q-ma-md">
+        <h4 class="q-mx-md q-my-sm text-white">
+          <q-icon name="redeem" size="2rem" class="q-pa-md" />Free games on
           <b>bot</b>
-        </h5>
+        </h4>
 
         <div class="card-container row justify-evenly">
           <q-card
-            class="free-card q-ma-sm"
+            class="free-card"
             v-for="card in freeCards"
             :key="card.id"
             flat
@@ -135,107 +139,118 @@
               {{ card.price }}
             </q-card-section>
             <q-card-section class="q-mb-md">
-              <q-card-text class="q-py-none">
+              <q-card-text class="card-developer q-py-none">
                 {{ card.developer }}
               </q-card-text>
             </q-card-section>
+            <q-card-section
+              ><q-btn
+                icon="redeem"
+                rounded
+                class="claim-btn full-width text-white"
+                >CLAIM</q-btn
+              ></q-card-section
+            >
           </q-card>
         </div>
       </div>
     </div>
   </section>
   <section>
-    <div class="sec-3 q-pa-md">
-      <div class="q-pa-md">
-        <div style="min-width: 100vh">
-          <q-tabs
+    <div class="sec-3 q-mt-xl q-mx-md">
+      <div class="q-pa-md" style="min-width: 100vh">
+        <q-tabs
+          v-model="tab"
+          align="justify"
+          narrow-indicator
+          class="tab q-pa-md text-lg q-mb-md"
+        >
+          <q-tab
+            no-caps
+            class="text-lg"
+            name="popularRelease"
+            label="Popular New Releases"
+          />
+          <q-tab class="text-lg" name="upcoming" label="Upcoming" no-caps />
+        </q-tabs>
+
+        <div class="q-gutter-y-sm">
+          <q-tab-panels
             v-model="tab"
-            align="justify"
-            narrow-indicator
-            class="q-mb-lg text-lg"
+            animated
+            transition-prev="scale"
+            transition-next="scale"
+            class="tab-panel-container"
           >
-            <q-tab
-              no-caps
-              class="text-lg"
-              name="popularRelease"
-              label="Popular New Releases"
-            />
-            <q-tab class="text-lg" name="upcoming" label="Upcoming" no-caps />
-          </q-tabs>
+            <q-tab-panel name="popularRelease">
+              <div class="card-container row justify-evenly">
+                <q-card
+                  class="tab-card q-pa-md column justify-evenly"
+                  v-for="card in cards"
+                  :key="card.id"
+                  flat
+                >
+                  <q-img class="card-image" :src="card.image" />
+                  <q-card-section class="q-py-sm q-px-none">
+                    <q-card-title class="card-title">
+                      {{ card.title }}
+                    </q-card-title>
+                  </q-card-section>
+                  <q-card-section class="card-price q-pa-none">
+                    {{ card.price }}
+                  </q-card-section>
+                  <q-card-section class="q-py-xs q-pl-none">
+                    <q-card-text class="card-developer">
+                      {{ card.developer }}
+                    </q-card-text>
+                  </q-card-section>
+                </q-card>
+              </div>
+            </q-tab-panel>
 
-          <div class="q-gutter-y-sm">
-            <q-tab-panels
-              v-model="tab"
-              animated
-              transition-prev="scale"
-              transition-next="scale"
-              class="tab-panel-container"
-            >
-              <q-tab-panel name="popularRelease">
-                <div class="card-container row justify-evenly">
-                  <q-card
-                    class="tab-card q-pa-md column justify-evenly q-mb-md"
-                    v-for="card in cards"
-                    :key="card.id"
-                    flat
-                  >
-                    <q-img class="card-image" :src="card.image" />
-                    <q-card-section class="q-py-sm q-px-none">
-                      <q-card-title class="card-title">
-                        {{ card.title }}
-                      </q-card-title>
-                    </q-card-section>
-                    <q-card-section class="card-price q-pa-none">
-                      {{ card.price }}
-                    </q-card-section>
-                    <q-card-section class="q-py-xs q-pl-none">
-                      <q-card-text class="">
-                        {{ card.developer }}
-                      </q-card-text>
-                    </q-card-section>
-                  </q-card>
-                </div>
-              </q-tab-panel>
-
-              <q-tab-panel name="upcoming">
-                <div class="card-container row justify-evenly">
-                  <q-card
-                    class="tab-card q-pa-md column justify-evenly q-mb-md"
-                    v-for="card in cards"
-                    :key="card.id"
-                    flat
-                  >
-                    <q-img class="card-image" :src="card.image" />
-                    <q-card-section class="q-py-sm q-px-none">
-                      <q-card-title class="card-title">
-                        {{ card.title }}
-                      </q-card-title>
-                    </q-card-section>
-                    <q-card-section class="card-price q-pa-none">
-                      {{ card.price }}
-                    </q-card-section>
-                    <q-card-section class="q-py-xs q-pl-none">
-                      <q-card-text class="">
-                        {{ card.developer }}
-                      </q-card-text>
-                    </q-card-section>
-                  </q-card>
-                </div>
-              </q-tab-panel>
-            </q-tab-panels>
-          </div>
+            <q-tab-panel name="upcoming">
+              <div class="card-container row justify-evenly">
+                <q-card
+                  class="tab-card q-pa-md column justify-evenly q-mb-md"
+                  v-for="card in cards"
+                  :key="card.id"
+                  flat
+                >
+                  <q-img class="card-image" :src="card.image" />
+                  <q-card-section class="q-py-sm q-px-none">
+                    <q-card-title class="card-title">
+                      {{ card.title }}
+                    </q-card-title>
+                  </q-card-section>
+                  <q-card-section class="card-price q-pa-none">
+                    {{ card.price }}
+                  </q-card-section>
+                  <q-card-section class="q-py-xs q-pl-none">
+                    <q-card-text class="card-developer">
+                      {{ card.developer }}
+                    </q-card-text>
+                  </q-card-section>
+                </q-card>
+              </div>
+            </q-tab-panel>
+          </q-tab-panels>
         </div>
       </div>
-      <div class="footer q-pa-xl bg-dark">
+      <div class="footer q-pa-md">
         <div>
-          <q-separator></q-separator>
-          <h3 class="text-weight-bold">bot</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
-            molestias possimus illo quos ab accusamus, excepturi rem nesciunt
-            nihil praesentium error sequi cupiditate necessitatibus, assumenda
-            minus. Dignissimos officiis cum perferendis?
-          </p>
+          <h3 class="text-weight-bold q-mb-sm">bot</h3>
+          <q-separator />
+          <div class="column justify-center">
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dolores molestias possimus illo quos ab accusamus, excepturi rem
+                nesciunt nihil praesentium error sequi cupiditate
+                necessitatibus, assumenda minus. Dignissimos officiis cum
+                perferendis?
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
